2a8599249a6c9d57b38c1d5660280c808fd136d3
shimanovAV <aleksandra.shimanovich@mail.ru>
2020-06-18 00:40:58 +0300

    change resources order

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
 
   resources :posts, only: [:create, :ips, :index] do
-    resource :rates, only: [:create]
     get '/:count', to: 'posts#index', on: :collection
-    get '/ips', to: 'posts#ips', on: :collection
+    get '/ips', to: 'posts#ips'
+    resource :rates, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
